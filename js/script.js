d69230423aa76d38b055c0dab63e790d77514366
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-26 23:22:58 +0200

    Prettifed everything

--- js/script.js
+++ js/script.js
@@ -1,71 +1,68 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav();
+  $(".sidenav").sidenav();
 });
 
 let amtOfSteps = 2;
 
-const input = document.getElementById('stepsToRepro1');
-input.addEventListener('input', updateValue);
+const input = document.getElementById("stepsToRepro1");
+input.addEventListener("input", updateValue);
 
 function updateValue(e) {
-    var x = document.createElement("TEXTAREA")
-    x.setAttribute("class", "materialize-textarea");
-    x.setAttribute("id", "stepsToRepro" + amtOfSteps)
-    x.addEventListener("input", updateValue);
+  var x = document.createElement("TEXTAREA");
+  x.setAttribute("class", "materialize-textarea");
+  x.setAttribute("id", "stepsToRepro" + amtOfSteps);
+  x.addEventListener("input", updateValue);
 
-    document.getElementById("steps").appendChild(x);
-    document.getElementById("stepsToRepro" + (amtOfSteps - 1)).removeEventListener("input", updateValue);
+  document.getElementById("steps").appendChild(x);
+  document.getElementById("stepsToRepro" + (amtOfSteps - 1)).removeEventListener("input", updateValue);
 
-    amtOfSteps++;
+  amtOfSteps++;
 }
 var removeStr = window.setInterval(callbackSTR, 100);
 function callbackSTR() {
-    for (let i = 1; i < amtOfSteps - 1; i++) {
-        if (document.getElementById("stepsToRepro" + i).value == "") {
-            document.getElementById("stepsToRepro" + (i + 1)).remove();
-            amtOfSteps--;
+  for (let i = 1; i < amtOfSteps - 1; i++) {
+    if (document.getElementById("stepsToRepro" + i).value == "") {
+      document.getElementById("stepsToRepro" + (i + 1)).remove();
+      amtOfSteps--;
 
-            const input = document.getElementById('stepsToRepro' + (amtOfSteps - 1));
-            input.addEventListener('input', updateValue);
-        }
-
+      const input = document.getElementById("stepsToRepro" + (amtOfSteps - 1));
+      input.addEventListener("input", updateValue);
     }
+  }
 }
 
 var intervalID = window.setInterval(myCallback, 1000);
 
 function myCallback() {
-    var x = document.getElementById("command")
-    x.innerHTML = "Missing "
-    if (document.getElementById("shortDesc").value == "") x.innerHTML += "Short Description, "
-    if (amtOfSteps - 2 == 0) x.innerHTML += "Steps to Reproduce, "
-    if (document.getElementById("expected").value == "") x.innerHTML += "Expected Result, "
-    if (document.getElementById("actual").value == "") x.innerHTML += "Actual Result, "
-    if (document.getElementById("clientVers").value == "") x.innerHTML += "Client Version, "
-    if (document.getElementById("systemVers").value == "") x.innerHTML += "System Settings"
+  var x = document.getElementById("command");
+  x.innerHTML = "Missing ";
+  if (document.getElementById("shortDesc").value == "") x.innerHTML += "Short Description, ";
+  if (amtOfSteps - 2 == 0) x.innerHTML += "Steps to Reproduce, ";
+  if (document.getElementById("expected").value == "") x.innerHTML += "Expected Result, ";
+  if (document.getElementById("actual").value == "") x.innerHTML += "Actual Result, ";
+  if (document.getElementById("clientVers").value == "") x.innerHTML += "Client Version, ";
+  if (document.getElementById("systemVers").value == "") x.innerHTML += "System Settings";
 
-    if (x.innerHTML == "Missing ") {
-        x.innerHTML = "!submit "
-        x.innerHTML += document.getElementById("shortDesc").value + " | "// Short Desc
-        x.innerHTML += "Steps to Reproduce: - "
+  if (x.innerHTML == "Missing ") {
+    x.innerHTML = "!submit ";
+    x.innerHTML += document.getElementById("shortDesc").value + " | "; // Short Desc
+    x.innerHTML += "Steps to Reproduce: - ";
 
-        for (let i = 1; i < amtOfSteps - 1; i++) {
-            if (i == amtOfSteps - 2) x.innerHTML += document.getElementById("stepsToRepro" + i).value
-            else x.innerHTML += document.getElementById("stepsToRepro" + i).value + " - "
-        }
-
-        x.innerHTML += " Expected Result: "
-        x.innerHTML += document.getElementById("expected").value
-
-        x.innerHTML += " Actual Result: "
-        x.innerHTML += document.getElementById("actual").value
-
-        x.innerHTML += " Client Settings: "
-        x.innerHTML += document.getElementById("clientVers").value
-
-        x.innerHTML += " System Settings: "
-        x.innerHTML += document.getElementById("systemVers").value
-
+    for (let i = 1; i < amtOfSteps - 1; i++) {
+      if (i == amtOfSteps - 2) x.innerHTML += document.getElementById("stepsToRepro" + i).value;
+      else x.innerHTML += document.getElementById("stepsToRepro" + i).value + " - ";
     }
 
+    x.innerHTML += " Expected Result: ";
+    x.innerHTML += document.getElementById("expected").value;
+
+    x.innerHTML += " Actual Result: ";
+    x.innerHTML += document.getElementById("actual").value;
+
+    x.innerHTML += " Client Settings: ";
+    x.innerHTML += document.getElementById("clientVers").value;
+
+    x.innerHTML += " System Settings: ";
+    x.innerHTML += document.getElementById("systemVers").value;
+  }
 }
