3d46c5fd7d8c29fac1f5e60c59aa1a30f354c746
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-26 11:25:51 +0200

    removed boostrap added materializecss, new navbar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+/*
+!submit This is a Short Description |
+Steps to Reproduce: - Step 1 - Step 2 - Step 3 - Step 4
+Expected Result: This is a Expected Result
+Actual Result: This is a Actual Result
+Client Settings: This is a Client Version
+System Settings: This is a System Settings
+*/
