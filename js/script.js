317708a41b404a334c5308af078e652d22a1eb9e
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-26 16:01:56 +0200

    Added steps to repro & short desc

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
+let amtOfSteps = 2;
+
+const input = document.getElementById('stepsToRepro1');
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+    var x = document.createElement("TEXTAREA")
+    x.setAttribute("class", "materialize-textarea");
+    x.setAttribute("id", "stepsToRepro" + amtOfSteps)
+    x.addEventListener("input", updateValue);
+
+    document.getElementById("steps").appendChild(x);
+    document.getElementById("stepsToRepro" + (amtOfSteps - 1)).removeEventListener("input", updateValue);
+
+    amtOfSteps++;
+
+
+}
 /*
 !submit This is a Short Description |
 Steps to Reproduce: - Step 1 - Step 2 - Step 3 - Step 4
