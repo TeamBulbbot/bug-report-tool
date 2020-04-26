0508330d3933ff73aa505b20c277972b63c1d38e
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-26 16:48:40 +0200

    Added a lot of new stuff that i dont wanna type

--- js/script.js
+++ js/script.js
@@ -18,11 +18,41 @@ function updateValue(e) {
 
     amtOfSteps++;
 }
-/*
-!submit This is a Short Description |
-Steps to Reproduce: - Step 1 - Step 2 - Step 3 - Step 4
-Expected Result: This is a Expected Result
-Actual Result: This is a Actual Result
-Client Settings: This is a Client Version
-System Settings: This is a System Settings
-*/
+
+var intervalID = window.setInterval(myCallback, 500);
+
+function myCallback() {
+    var x = document.getElementById("command")
+    x.innerHTML = "Missing "
+    if (document.getElementById("shortDesc").value == "") x.innerHTML += "Short Description, "
+    if (amtOfSteps - 2 == 0) x.innerHTML += "Steps to Reproduce, "
+    if (document.getElementById("expected").value == "") x.innerHTML += "Expected Result, "
+    if (document.getElementById("actual").value == "") x.innerHTML += "Actual Result, "
+    if (document.getElementById("clientVers").value == "") x.innerHTML += "Client Version, "
+    if (document.getElementById("systemVers").value == "") x.innerHTML += "System Settings"
+
+    if (x.innerHTML == "Missing ") {
+        x.innerHTML = "!submit "
+        x.innerHTML += document.getElementById("shortDesc").value + " | "// Short Desc
+        x.innerHTML += "Steps to Reproduce: - "
+
+        for (let i = 1; i < amtOfSteps - 1; i++) {
+            if (i == amtOfSteps - 2) x.innerHTML += document.getElementById("stepsToRepro" + i).value
+            else x.innerHTML += document.getElementById("stepsToRepro" + i).value + " - "
+        }
+
+        x.innerHTML += " Expected Result: "
+        x.innerHTML += document.getElementById("expected").value
+
+        x.innerHTML += " Actual Result: "
+        x.innerHTML += document.getElementById("actual").value
+
+        x.innerHTML += " Client Settings: "
+        x.innerHTML += document.getElementById("clientVers").value
+
+        x.innerHTML += " System Settings: "
+        x.innerHTML += document.getElementById("systemVers").value
+
+    }
+
+}
