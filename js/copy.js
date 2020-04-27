19df6a0e60de07be1f961d1a525b2a558a9f8e13
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-27 21:18:16 +0200

    Removed console.log

--- js/copy.js
+++ js/copy.js
@@ -27,14 +27,7 @@ function copyTextToClipboard(text) {
     fallbackCopyTextToClipboard(text);
     return;
   }
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log("Async: Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Async: Could not copy text: ", err);
-    }
-  );
+  navigator.clipboard.writeText(text);
 }
 
 var copyBtn = document.getElementById("copy");
