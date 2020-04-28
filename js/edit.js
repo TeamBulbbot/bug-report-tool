6f7bd53e966f19db621342a0812f6be5c6592f4e
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-28 20:09:44 +0200

    Created the Edit (dark theme) page

--- js/edit.js
+++ js/edit.js
@@ -4,3 +4,29 @@ $(document).ready(function() {
   $("select").formSelect();
   $(".dropdown-trigger").dropdown();
 });
+
+var intervalID = window.setInterval(myCallback, 1000);
+
+
+function myCallback() {
+  var x = document.getElementById("command");
+  x.innerHTML = "Missing ";
+
+  if (document.getElementById("reportID").value == "") x.innerHTML += "Report ID, ";
+  if (document.getElementById("newContent").value == "") x.innerHTML += "New content";
+
+  if (x.innerHTML == "Missing ") {
+    x.innerHTML = "!edit ";
+    x.innerHTML += document.getElementById("reportID").value;
+
+    if (document.getElementById("section").value == 1) x.innerHTML += " | short description | ";
+    if (document.getElementById("section").value == 2) x.innerHTML += " | Steps to Reproduce | ";
+    if (document.getElementById("section").value == 3) x.innerHTML += " | expected | ";
+    if (document.getElementById("section").value == 4) x.innerHTML += " | actual | ";
+    if (document.getElementById("section").value == 5) x.innerHTML += " | client | ";
+    if (document.getElementById("section").value == 6) x.innerHTML += " | system | ";
+
+    x.innerHTML += document.getElementById("newContent").value;
+
+  }
+}
