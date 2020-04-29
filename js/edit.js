d7ea38134f0122ef330e6bf08861a64b40f80770
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-04-29 13:19:45 +0200

    Fixed issue #12

--- js/edit.js
+++ js/edit.js
@@ -3,10 +3,64 @@ $(document).ready(function() {
   $(".modal").modal();
   $("select").formSelect();
   $(".dropdown-trigger").dropdown();
-  $("textarea#reportID, textarea#newContent").characterCounter();
+  $("textarea#reportID, textarea#newContent, textarea#stepsToRepro1").characterCounter();
 });
 
 var intervalID = window.setInterval(myCallback, 1000);
+var strInternval = window.setInterval(checkIfSTRISActive, 1000);
+
+var removeStr = window.setInterval(callbackSTR, 100);
+let amtOfSteps = 2;
+
+const input = document.getElementById("stepsToRepro1");
+input.addEventListener("input", updateValue);
+
+function checkIfSTRISActive() {
+  if (document.getElementById("section").value == 2) {
+    document.getElementById("strField").style.display = 'block';
+    document.getElementById("other").style.display = 'none';
+
+
+
+  } else {
+    document.getElementById("strField").style.display = 'none';
+    document.getElementById("other").style.display = 'block';
+
+  }
+}
+
+
+function updateValue(e) {
+  var x = document.createElement("TEXTAREA");
+  x.setAttribute("class", "materialize-textarea");
+  x.setAttribute("id", "stepsToRepro" + amtOfSteps);
+  x.setAttribute("data-length", "75");
+  x.setAttribute("maxlength", "75");
+  x.addEventListener("input", updateValue);
+
+  document.getElementById("steps").appendChild(x);
+
+  $(document).ready(function() {
+    $("textarea#stepsToRepro" + (amtOfSteps - 1)).characterCounter();
+  });
+
+  document.getElementById("stepsToRepro" + (amtOfSteps - 1)).removeEventListener("input", updateValue);
+
+  amtOfSteps++;
+}
+
+function callbackSTR() {
+  for (let i = 1; i < amtOfSteps - 1; i++) {
+    if (document.getElementById("stepsToRepro" + i).value == "") {
+      document.getElementById("stepsToRepro" + (i + 1)).remove();
+      document.getElementById("steps").getElementsByClassName("character-counter")[i - 1].remove();
+      amtOfSteps--;
+
+      const input = document.getElementById("stepsToRepro" + (amtOfSteps - 1));
+      input.addEventListener("input", updateValue);
+    }
+  }
+}
 
 
 function myCallback() {
